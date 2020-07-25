5372de15c969d9addeb18510f2d7f1b8be79e028
raulsanba <nogasanrl@gmail.com>
2020-07-25 01:06:07 +0100

    Plugin working locally

--- InventorPartMakerDA2/InventorPartMakerDA2Plugin/SampleAutomation.cs
+++ InventorPartMakerDA2/InventorPartMakerDA2Plugin/SampleAutomation.cs
@@ -45,146 +45,96 @@ namespace InventorPartMakerDA2Plugin
 
         public void RunWithArguments(Document doc, NameValueMap map)
         {
-            //LogTrace("Processing " + doc.FullFileName);
-
-            //try
-            //{
-            //    if (doc.DocumentType == DocumentTypeEnum.kPartDocumentObject)
-            //    {
-            //        using (new HeartBeat())
-            //        {
-            //            // TODO: handle the Inventor part here
-            //        }
-            //    }
-            //    else if (doc.DocumentType == DocumentTypeEnum.kAssemblyDocumentObject) // Assembly.
-            //    {
-            //        using (new HeartBeat())
-            //        {
-            //            // TODO: handle the Inventor assembly here
-            //        }
-            //    }
-            //}
-            //catch (Exception e)
-            //{
-            //    LogError("Processing failed. " + e.ToString());
-            //}
-            LogTrace("Initialiting");
+            
+            //LogTrace("Initialiting");
             PartDocument oPartDoc = (PartDocument)inventorApplication.Documents.Add(DocumentTypeEnum.kPartDocumentObject, inventorApplication.FileManager.GetTemplateFile(DocumentTypeEnum.kPartDocumentObject), true);
             LogTrace("Part template opened");
             TransientGeometry oTG = inventorApplication.TransientGeometry;
             PartComponentDefinition oPartComDef = oPartDoc.ComponentDefinition;
             UserParameters oParams = oPartComDef.Parameters.UserParameters;
-            oParams.AddByExpression("width", "50", UnitsTypeEnum.kMillimeterLengthUnits);
-            oParams.AddByExpression("height", "10", UnitsTypeEnum.kMillimeterLengthUnits);
-            oParams.AddByExpression("length", "10", UnitsTypeEnum.kMillimeterLengthUnits);
-            LogTrace("Standard parameters created");
-
-
-            Point2d[] oPoints = new Point2d[4];
-
-
-            oPoints[0] = oTG.CreatePoint2d(0, 0);
-            oPoints[1] = oTG.CreatePoint2d(0, oPartComDef.Parameters.GetValueFromExpression("width", UnitsTypeEnum.kMillimeterLengthUnits));
-            oPoints[2] = oTG.CreatePoint2d(oPartComDef.Parameters.GetValueFromExpression("height", UnitsTypeEnum.kMillimeterLengthUnits), 0);
-            oPoints[3] = oTG.CreatePoint2d(oPartComDef.Parameters.GetValueFromExpression("height", UnitsTypeEnum.kMillimeterLengthUnits), oPartComDef.Parameters.GetValueFromExpression("width", UnitsTypeEnum.kMillimeterLengthUnits));
-            LogTrace("Inventor points created");
-            LogTrace("Initiating sketch creation...");
-            PlanarSketch oSketch = oPartComDef.Sketches.Add(oPartComDef.WorkPlanes[1]);
-            SketchPoint[] osPoints = new SketchPoint[4];
-            SketchLine[] oLines = new SketchLine[4];
-
-            osPoints[0] = oSketch.SketchPoints.Add(oPoints[0]);
-            osPoints[1] = oSketch.SketchPoints.Add(oPoints[1]);
-            osPoints[2] = oSketch.SketchPoints.Add(oPoints[2]);
-            osPoints[3] = oSketch.SketchPoints.Add(oPoints[3]);
-
-            oLines[0] = oSketch.SketchLines.AddByTwoPoints(osPoints[0], osPoints[1]);
-            oLines[1] = oSketch.SketchLines.AddByTwoPoints(oLines[0].EndSketchPoint, osPoints[3]);
-            oLines[2] = oSketch.SketchLines.AddByTwoPoints(oLines[1].EndSketchPoint, osPoints[2]);
-            oLines[3] = oSketch.SketchLines.AddByTwoPoints(oLines[2].EndSketchPoint, oLines[0].StartSketchPoint);
-            LogTrace("Sketch created, adding dimensions");
-            oSketch.DimensionConstraints.AddTwoPointDistance(osPoints[0], osPoints[1], DimensionOrientationEnum.kAlignedDim, oPoints[1]); //d0//
-            oSketch.DimensionConstraints.AddTwoPointDistance(osPoints[1], osPoints[3], DimensionOrientationEnum.kAlignedDim, oPoints[3]); //d1//
-            LogTrace("Dimensions added to the sketch, changing standard values for User parameter values");
-
-            var inventorMParams = oPartComDef.Parameters.ModelParameters;
-            foreach (ModelParameter mParam in inventorMParams)
-            {
-                if (mParam.Name.Contains("d0"))
-                {
-                    mParam.Expression = "width";
-                }
-                else if (mParam.Name.Contains("d1"))
-                {
-                    mParam.Expression = "height";
-                }
-
-            }
-
-            LogTrace("Dimensions with user parameter values created, starting extrude operation");
-
-            //oSketch.DimensionConstraints.AddTwoPointDistance(oPoints[0], oPoints[1], DimensionOrientationEnum.kAlignedDim, "width", true);
-            Profile oProfile = oSketch.Profiles.AddForSolid();
-
-            ExtrudeDefinition oExtrudeDef = oPartComDef.Features.ExtrudeFeatures.CreateExtrudeDefinition(oProfile, PartFeatureOperationEnum.kJoinOperation);
-            oExtrudeDef.SetDistanceExtent(oPartComDef.Parameters.UserParameters.AddByExpression("length", "length", UnitsTypeEnum.kMillimeterLengthUnits), PartFeatureExtentDirectionEnum.kPositiveExtentDirection);
-
-            ExtrudeFeature oExtrude = oPartComDef.Features.ExtrudeFeatures.Add(oExtrudeDef);
-            //oExtrude.FeatureDimensions[1].Parameter.Name = "length";
-            LogTrace("Extrude operation finished");
+           
             XmlDocument xmlDoc = new XmlDocument();
             string currentDir = System.IO.Directory.GetCurrentDirectory();
             string projectDir = Directory.GetParent(currentDir).Parent.FullName;
             LogTrace("Reading XML input file from " + projectDir);
-            xmlDoc.Load(System.IO.Path.Combine(projectDir, "React-BIM-output.xml"));
+            xmlDoc.Load(System.IO.Path.Combine(projectDir, "react-test-output.xml"));
             //xmlDoc.Load("C:\\webapps\\IpartCreator\\React-BIM-output.xml");
-            XmlNodeList memberNodeList = xmlDoc.GetElementsByTagName("Member");
-            XmlNodeList partNumberNodeList = xmlDoc.GetElementsByTagName("PartNumber");
-            XmlNodeList widthNodeList = xmlDoc.GetElementsByTagName("width");
-            XmlNodeList heightNodeList = xmlDoc.GetElementsByTagName("height");
-            XmlNodeList lengthNodeList = xmlDoc.GetElementsByTagName("length");
-            LogTrace("XML tag values imported");
-            LogTrace("Creating Part");
+            XmlNodeList FloorList = xmlDoc.DocumentElement.SelectNodes("/Building/Floors/Floor");
+            XmlNodeList FloorPointList = xmlDoc.DocumentElement.SelectNodes("/Building/Floors/Floor/BoundaryPoints/Point");
+            XmlNodeList ComponentName = xmlDoc.DocumentElement.SelectNodes("/Building/Floors/Floor/ComponentName");
+            XmlNodeList PointX = xmlDoc.DocumentElement.SelectNodes("/Building/Floors/Floor/BoundaryPoints/Point/X");
+            XmlNodeList PointY = xmlDoc.DocumentElement.SelectNodes("/Building/Floors/Floor/BoundaryPoints/Point/Y");
+            XmlNodeList PointZ = xmlDoc.DocumentElement.SelectNodes("/Building/Floors/Floor/BoundaryPoints/Point/Z");
+
             
-            foreach(UserParameter oParam in oParams)
+
+            for (int i=0; i < FloorList.Count; i++)
             {
-                if (oParam.Name.Contains("width"))
+                //oParams.AddByExpression("ComponentName" + i, ComponentName[i].InnerText, UnitsTypeEnum.kUnitlessUnits);
+               
+                
+                int numPoint = FloorPointList.Count / FloorList.Count;
+                
+                Point2d[] oPoints = new Point2d[numPoint];  
+                SketchPoint[] osPoints = new SketchPoint[numPoint];
+              
+                for (int j = 0; j < numPoint; j++)
                 {
-                    oParam.Expression = widthNodeList.;
+
+                    oParams.AddByExpression("PointX" + j, PointX[j].InnerText, UnitsTypeEnum.kMillimeterLengthUnits);
+                    oParams.AddByExpression("PointY" + j, PointY[j].InnerText, UnitsTypeEnum.kMillimeterLengthUnits);
+                    oParams.AddByExpression("PointZ" + j, PointZ[j].InnerText, UnitsTypeEnum.kMillimeterLengthUnits);
+                    
+                    oPoints[j] = oTG.CreatePoint2d(oPartComDef.Parameters.GetValueFromExpression("PointX" + j, UnitsTypeEnum.kMillimeterLengthUnits), oPartComDef.Parameters.GetValueFromExpression("PointY" + j, UnitsTypeEnum.kMillimeterLengthUnits));
+                   
+
                 }
+
+                SketchLine[] oLines = new SketchLine[numPoint];
+                PlanarSketch oSketch = oPartComDef.Sketches.Add(oPartComDef.WorkPlanes[2]);
+                osPoints[0] = oSketch.SketchPoints.Add(oPoints[0]);
+                osPoints[1] = oSketch.SketchPoints.Add(oPoints[1]);
+                osPoints[2] = oSketch.SketchPoints.Add(oPoints[2]);
+                osPoints[3] = oSketch.SketchPoints.Add(oPoints[3]);
+
+                oLines[0] = oSketch.SketchLines.AddByTwoPoints(osPoints[0], osPoints[1]);
+                oLines[1] = oSketch.SketchLines.AddByTwoPoints(oLines[0].EndSketchPoint, osPoints[2]);
+                oLines[2] = oSketch.SketchLines.AddByTwoPoints(oLines[1].EndSketchPoint, osPoints[3]);
+                oLines[3] = oSketch.SketchLines.AddByTwoPoints(oLines[2].EndSketchPoint, oLines[0].StartSketchPoint);
+
+                oSketch.DimensionConstraints.AddTwoPointDistance(osPoints[0], osPoints[1], DimensionOrientationEnum.kAlignedDim, oPoints[1]); //d0//
+                oSketch.DimensionConstraints.AddTwoPointDistance(osPoints[1], osPoints[2], DimensionOrientationEnum.kAlignedDim, oPoints[3]); //d1//
+
+                Profile oProfile = oSketch.Profiles.AddForSolid();
+                ExtrudeDefinition oExtrudeDef = oPartComDef.Features.ExtrudeFeatures.CreateExtrudeDefinition(oProfile, PartFeatureOperationEnum.kJoinOperation);
+                oExtrudeDef.SetDistanceExtent(oPartComDef.Parameters.UserParameters.AddByExpression("length","8", UnitsTypeEnum.kMillimeterLengthUnits), PartFeatureExtentDirectionEnum.kPositiveExtentDirection);
+                ExtrudeFeature oExtrude = oPartComDef.Features.ExtrudeFeatures.Add(oExtrudeDef);
+
+                string PartPath = projectDir + "/results/" + ComponentName[i].InnerText + i + ".ipt";
+                oPartDoc.SaveAs(PartPath, false);
+
+                oExtrude.Delete();
+                
             }
-            
 
+            oPartDoc.Close(false);
 
-
-          
-            LogTrace("Excel created");
-            oPartDoc.Update();
-            string iPartPath = projectDir + "/results/bimipart.ipt";
-            int iNumRows = oFactory.TableRows.Count;
-            LogTrace("Saving iPart to " + iPartPath);
-            oPartDoc.SaveAs(iPartPath, false);
-
-            LogTrace("Opening new assembly template");
             AssemblyDocument oAssyDoc = (AssemblyDocument)inventorApplication.Documents.Add(DocumentTypeEnum.kAssemblyDocumentObject, inventorApplication.FileManager.GetTemplateFile(DocumentTypeEnum.kAssemblyDocumentObject), true);
             AssemblyComponentDefinition oAssyComDef = oAssyDoc.ComponentDefinition;
             ComponentOccurrences oAssyCompOccs = oAssyComDef.Occurrences;
-            LogTrace("Creating Matrix");
             Matrix oPos = oTG.CreateMatrix();
             int oStep = 0;
-            int iRow;
-            LogTrace("Placing iPart members to assembly");
-            for (iRow = 1; iRow <= iNumRows; iRow++)
+            int icomp;
+            int numite = FloorPointList.Count / FloorList.Count;
+            for (icomp=0; icomp <= numite; icomp++)
             {
                 oStep = oStep + 150;
-
                 oPos.SetTranslation(oTG.CreateVector(oStep, oStep, 0), false);
-                ComponentOccurrence oOcc = oAssyCompOccs.AddiPartMember(iPartPath, oPos, iRow);
+                string PartPath = projectDir + "/results/" + ComponentName[icomp].InnerText + icomp + ".ipt";
+                ComponentOccurrence oOcc = oAssyCompOccs.Add(PartPath, oPos);
             }
-            string assyPath = projectDir + "/results/bimassy.iam";
-            LogTrace("Saving Assembly file to " + assyPath);
-            oAssyDoc.SaveAs(assyPath, false);
 
+            oAssyDoc.SaveAs(projectDir + "/results/result.iam", false);
         }
 
         #region Logging utilities
